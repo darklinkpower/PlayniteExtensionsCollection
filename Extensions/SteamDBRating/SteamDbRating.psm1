d1f28646522d2057113795ca4c7792ebf2bfedaf
Brandon <brandon_corchado1@hotmail.com>
2021-02-09 16:17:54 -0600

    SteamDbRating: Add localizations support

--- Extensions/SteamDBRating/SteamDbRating.psm1
+++ Extensions/SteamDBRating/SteamDbRating.psm1
@@ -5,7 +5,7 @@ function GetMainMenuItems
     )
 
     $menuItem1 = New-Object Playnite.SDK.Plugins.ScriptMainMenuItem
-    $menuItem1.Description = "Get rating for selected games"
+    $menuItem1.Description = [Playnite.SDK.ResourceProvider]::GetString("LOCMenuItemGetRatingSelectedGamesDescription")
     $menuItem1.FunctionName = "SteamDbRating"
     $menuItem1.MenuSection = "@SteamDB Rating"
 
@@ -14,6 +14,10 @@ function GetMainMenuItems
 
 function SteamDbRating
 {
+    param(
+        $scriptMainMenuItemActionArgs
+    )
+    
     # Set Url Templates
     $SteamApiReviewsTemplate =  "https://store.steampowered.com/appreviews/{0}?json=1&purchase_type=all&language=all"
     
@@ -22,7 +26,6 @@ function SteamDbRating
     
     # Set Counters
     $CountScoreAdded = 0
-    $CountErrors = 0
     
     foreach ($game in $GameDatabase) {
         #Clear variables
@@ -58,7 +61,6 @@ function SteamDbRating
                 $json = $downloadedString | ConvertFrom-Json
             } catch {
                 $__logger.Warn("SteamDbRating - `"$($game.name)`" information couldn't be downloaded")
-                $CountErrors++
                 continue
             }
             
@@ -66,7 +68,6 @@ function SteamDbRating
             if ( ($json.success -eq 2) -or ( $json.query_summary.total_reviews -eq 0) )
             {
                 $__logger.Warn("SteamDbRating - `"$($game.name)`" has been removed from Steam or doesn't have reviews")
-                $CountErrors++
                 continue
             }
             
@@ -90,21 +91,9 @@ function SteamDbRating
         {
             # Log Error if no Steam AppId and no steam link for games from other sources
             $__logger.Warn("SteamDbRating - `"$($game.name)`" no SteamId found to download information")
-            $CountErrors++
-            continue
         }
     }
     
-    # Error log
-    if ($CountErrors -gt 0)
-    {
-        $ErrorReport = "There were $CountErrors errors. Check Playnite log for details."
-    }
-    else
-    {
-        $ErrorReport = "There were no errors"
-    }
-    
     # Show finish dialogue with results
-    $PlayniteApi.Dialogs.ShowMessage("Number of processed games: $($GameDatabase.Count)`n`nGames that had a new Community Score added: $CountScoreAdded`n$ErrorReport", "SteamDB Rating - Results");
+    $PlayniteApi.Dialogs.ShowMessage(([Playnite.SDK.ResourceProvider]::GetString("LOCResultsMessage") -f $GameDatabase.Count, $CountScoreAdded), "SteamDB Rating")
 }
