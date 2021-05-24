6cdcef1784d12228203b9998cbc2c41bb374627c
Brandon <brandon_corchado1@hotmail.com>
2021-05-23 20:48:43 -0500

    DescriptionRemoveAboutGame update

--- Extensions/DescriptionRemoveAboutGame/DescriptionRemoveAboutGame.psm1
+++ Extensions/DescriptionRemoveAboutGame/DescriptionRemoveAboutGame.psm1
@@ -1,64 +1,78 @@
-function global:GetMainMenuItems
+function GetMainMenuItems
 {
-    param($menuArgs)
+    param(
+        $menuArgs
+    )
 
     $menuItem1 = New-Object Playnite.SDK.Plugins.ScriptMainMenuItem
-    $menuItem1.Description = "All games in database"
-    $menuItem1.FunctionName = "DescriptionRemoveAboutGameAll"
-    $menuItem1.MenuSection = "@Description remove before `"About Game`""
+    $menuItem1.Description = "Clean description of selected PC games"
+    $menuItem1.FunctionName = "Invoke-FormatSelectedGameDescriptions"
+    $menuItem1.MenuSection = "@Steam descriptions cleaner"
     
     $menuItem2 = New-Object Playnite.SDK.Plugins.ScriptMainMenuItem
-    $menuItem2.Description = "Only selected games"
-    $menuItem2.FunctionName = "DescriptionRemoveAboutGameSelected"
-    $menuItem2.MenuSection = "@Description remove before `"About Game`""
+    $menuItem2.Description = "Clean description of all PC games in database"
+    $menuItem2.FunctionName = "Invoke-FormatAllGameDescriptions"
+    $menuItem2.MenuSection = "@Steam descriptions cleaner"
     
     return $menuItem1, $menuItem2
 }
 
-function DescriptionRemoveAboutGameAll
+function Format-SteamDescription
 {
-    #Set GameDatabase
-    $GameDatabase = $PlayniteApi.Database.Games | Where-Object { ($_.description) -and ($_.platform.name -eq "PC") }
-
-    # Set counters
-    $ChangedCount = 0
-
-    # Regex
-    $regex = '(?:[\s\S]+)<h1>About the Game<\/h1>([\s\S]+)'
-
-    foreach ($game in $GameDatabase) {
-        $RegexMatch = ([regex]$regex).Matches($($game.description))
-        if ($RegexMatch.count -eq 1)
-        {
-            $game.description = '<h1>About the Game</h1>' + $RegexMatch.groups[1].value
-            $PlayniteApi.Database.Games.Update($game)
-            $ChangedCount++
-        }
+    param (
+        $game
+    )
+    
+    $descriptionChangedCount = 0
+    if ($null -eq $game.Description)
+    {
+        return $descriptionChangedCount
     }
-    # Show finish dialogue with results
-    $PlayniteApi.Dialogs.ShowMessage("Changed $ChangedCount games description ", "Description Remove `"About Game`"");
+
+    $regex = '(?:[\s\S]+)<h1>About the Game<\/h1>([\s\S]+)'
+    $RegexMatch = ([regex]$regex).Matches($game.description)
+    if ($RegexMatch.count -eq 1)
+    {
+        $game.description = '<h1>About the Game</h1>' + $RegexMatch.groups[1].value
+        $PlayniteApi.Database.Games.Update($game)
+        $descriptionChangedCount++
+        $__logger.Info("Cleaned description of `"$($game.name)`"")
+    }
+    return $descriptionChangedCount
 }
 
-function DescriptionRemoveAboutGameSelected
+function Invoke-FormatGamesCollectionDescription
 {
-    #Set GameDatabase
-    $GameDatabase = $PlayniteApi.Mainview.Selectedgames | Where-Object { ($_.description) -and ($_.platform.name -eq "PC") }
+    param (
+        $gameCollection
+    )
 
-    # Set counters
-    $ChangedCount = 0
-
-    # Regex
-    $regex = '(?:[\s\S]+)<h1>About the Game<\/h1>([\s\S]+)'
-
-    foreach ($game in $GameDatabase) {
-        $RegexMatch = ([regex]$regex).Matches($($game.description))
-        if ($RegexMatch.count -eq 1)
-        {
-            $game.description = '<h1>About the Game</h1>' + $RegexMatch.groups[1].value
-            $PlayniteApi.Database.Games.Update($game)
-            $ChangedCount++
-        }
+    $descriptionChangedCount = 0
+    foreach ($game in $gameCollection) {
+        $descriptionChanged = Format-SteamDescription $gameCollection
+        $descriptionChangedCount += $descriptionChanged
     }
+
     # Show finish dialogue with results
-    $PlayniteApi.Dialogs.ShowMessage("Changed $ChangedCount games description ", "Description Remove `"About Game`"");
+    $PlayniteApi.Dialogs.ShowMessage("Changed $descriptionChangedCount games description ", "Description Remove `"About Game`"")
+}
+
+function Invoke-FormatAllGameDescriptions
+{
+    param(
+        $scriptMainMenuItemActionArgs
+    )
+    
+    $gameCollection = $PlayniteApi.Database.Games | Where-Object { ($_.description) -and ($_.platform.name -eq "PC") }
+    Invoke-FormatGamesCollectionDescription $gameCollection
+}
+
+function Invoke-FormatSelectedGameDescriptions
+{
+    param(
+        $scriptMainMenuItemActionArgs
+    )
+    
+    $gameCollection = $PlayniteApi.Mainview.Selectedgames | Where-Object { ($_.description) -and ($_.platform.name -eq "PC") }
+    Invoke-FormatGamesCollectionDescription $gameCollection
 }
