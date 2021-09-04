885e245ba8bd6944f60f4e0461a7ab1d6907da14
Brandon <brandon_corchado1@hotmail.com>
2021-09-04 08:38:44 -0500

    Update manifests version and add Crowdin link

--- source/Generic/LogOpen/LogOpen.psm1
+++ source/Generic/LogOpen/LogOpen.psm1
@@ -8,7 +8,11 @@ function GetMainMenuItems
     $menuItem1.Description = [Playnite.SDK.ResourceProvider]::GetString("LOCLog_Open_MenuItemOpenLogDescription")
     $menuItem1.FunctionName = "Open-Log"
 
-    return $menuItem1
+    $menuItem2 = New-Object Playnite.SDK.Plugins.ScriptMainMenuItem
+    $menuItem2.Description = [Playnite.SDK.ResourceProvider]::GetString("LOCLog_Open_MenuItemOpenExtensionsLogDescription")
+    $menuItem2.FunctionName = "Open-ExtensionsLog"
+
+    return $menuItem1, $menuItem2
 }
 
 function Open-Log
@@ -17,7 +21,20 @@ function Open-Log
         $scriptMainMenuItemActionArgs
     )
     
-    $logPath = Join-Path $PlayniteApi.Paths.ConfigurationPath -ChildPath "Playnite.log"
+    $logPath = Join-Path $PlayniteApi.Paths.ConfigurationPath -ChildPath "playnite.log"
+    if (Test-Path $logPath)
+    {
+        Start-Process $logPath
+    }
+}
+
+function Open-ExtensionsLog
+{
+    param(
+        $scriptMainMenuItemActionArgs
+    )
+    
+    $logPath = Join-Path $PlayniteApi.Paths.ConfigurationPath -ChildPath "extensions.log"
     if (Test-Path $logPath)
     {
         Start-Process $logPath
