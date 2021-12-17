30cdd2199c2a9ff5bae744b2cc1a38374ab71eb0
Brandon <brandon_corchado1@hotmail.com>
2021-12-16 20:26:39 -0600

    ResolutionChanger: Remove unused reference

--- source/Generic/ResolutionChanger/ResolutionChangerSettings.cs
+++ source/Generic/ResolutionChanger/ResolutionChangerSettings.cs
@@ -5,7 +5,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using static ResolutionChanger.DisplayUtilities;
 
 namespace ResolutionChanger
 {
