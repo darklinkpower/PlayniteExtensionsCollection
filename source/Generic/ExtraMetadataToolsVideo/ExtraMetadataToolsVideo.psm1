330af51d4b988bf61b3832d16e04008306e05549
Brandon <brandon_corchado1@hotmail.com>
2022-03-07 17:21:08 -0600

    Remove deprecated extensions

--- source/Generic/ExtraMetadataToolsVideo/ExtraMetadataToolsVideo.psm1
+++
@@ -1,4 +0,0 @@
-function OnApplicationStarted
-{
-    $PlayniteApi.Dialogs.ShowMessage("The `"Extra Metadata Tools Video`" extension has had all its functionality removed and must be uninstalled from your installation.`n`nRemoved functionality has been reimplemented and improved in the `"Extra Metadata Loader`" extension that can be installed from the Playnite addons Browser.`n`nThis extension has been labeled `"Extra Metadata deprecated extension. Uninstall!`" and can be uninstalled from the addons window.", "Extra Metadata Tools Video")
-}
