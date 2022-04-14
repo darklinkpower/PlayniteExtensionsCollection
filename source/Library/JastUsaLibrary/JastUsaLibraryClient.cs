3f7d728ef4a6f234fea5bfc4269104974c78b044
Brandon <brandon_corchado1@hotmail.com>
2022-04-14 14:34:17 -0500

    JastUsaLibrary:  Implement client open (Opens website in browser)

--- source/Library/JastUsaLibrary/JastUsaLibraryClient.cs
+++ source/Library/JastUsaLibrary/JastUsaLibraryClient.cs
@@ -1,7 +1,10 @@
 ﻿using Playnite.SDK;
+using PluginsCommon;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,10 +13,11 @@ namespace JastUsaLibrary
     public class JastUsaLibraryClient : LibraryClient
     {
         public override bool IsInstalled => true;
+        public override string Icon => Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"icon.png");
 
         public override void Open()
         {
-            throw new NotImplementedException();
+            ProcessStarter.StartUrl(@"https://jastusa.com/");
         }
     }
 }
