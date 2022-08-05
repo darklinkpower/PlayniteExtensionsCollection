68622ce7f2c34b4b9738de97995d015f79c2768b
Brandon <brandon_corchado1@hotmail.com>
2022-08-04 23:51:52 -0500

    PurchaseDateImporter progress

---
+++ source/Generic/PurchaseDateImporter/Models/EaAuthResponse.cs
@@ -0,0 +1,180 @@
+﻿using Playnite.SDK.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PurchaseDateImporter.Models
+{
+    public partial class EaAuthResponse
+    {
+        [SerializationPropertyName("access_token")]
+        public string AccessToken { get; set; }
+
+        [SerializationPropertyName("token_type")]
+        public string TokenType { get; set; }
+
+        [SerializationPropertyName("expires_in")]
+        public string ExpiresIn { get; set; }
+    }
+
+    public partial class EaIdentityResponse
+    {
+        [SerializationPropertyName("pid")]
+        public Pid Pid { get; set; }
+    }
+
+    public partial class Pid
+    {
+        [SerializationPropertyName("externalRefType")]
+        public string ExternalRefType { get; set; }
+
+        [SerializationPropertyName("externalRefValue")]
+        public string ExternalRefValue { get; set; }
+
+        [SerializationPropertyName("pidId")]
+        public long PidId { get; set; }
+
+        [SerializationPropertyName("email")]
+        public string Email { get; set; }
+
+        [SerializationPropertyName("emailStatus")]
+        public string EmailStatus { get; set; }
+
+        [SerializationPropertyName("strength")]
+        public string Strength { get; set; }
+
+        [SerializationPropertyName("dob")]
+        public DateTimeOffset Dob { get; set; }
+
+        [SerializationPropertyName("country")]
+        public string Country { get; set; }
+
+        [SerializationPropertyName("language")]
+        public string Language { get; set; }
+
+        [SerializationPropertyName("locale")]
+        public string Locale { get; set; }
+
+        [SerializationPropertyName("status")]
+        public string Status { get; set; }
+
+        [SerializationPropertyName("reasonCode")]
+        public string ReasonCode { get; set; }
+
+        [SerializationPropertyName("tosVersion")]
+        public string TosVersion { get; set; }
+
+        [SerializationPropertyName("parentalEmail")]
+        public string ParentalEmail { get; set; }
+
+        [SerializationPropertyName("thirdPartyOptin")]
+        public string ThirdPartyOptin { get; set; }
+
+        [SerializationPropertyName("globalOptin")]
+        public string GlobalOptin { get; set; }
+
+        [SerializationPropertyName("dateCreated")]
+        public string DateCreated { get; set; }
+
+        [SerializationPropertyName("dateModified")]
+        public string DateModified { get; set; }
+
+        [SerializationPropertyName("lastAuthDate")]
+        public string LastAuthDate { get; set; }
+
+        [SerializationPropertyName("registrationSource")]
+        public string RegistrationSource { get; set; }
+
+        [SerializationPropertyName("authenticationSource")]
+        public string AuthenticationSource { get; set; }
+
+        [SerializationPropertyName("showEmail")]
+        public string ShowEmail { get; set; }
+
+        [SerializationPropertyName("discoverableEmail")]
+        public string DiscoverableEmail { get; set; }
+
+        [SerializationPropertyName("anonymousPid")]
+        public string AnonymousPid { get; set; }
+
+        [SerializationPropertyName("underagePid")]
+        public string UnderagePid { get; set; }
+
+        [SerializationPropertyName("defaultBillingAddressUri")]
+        public string DefaultBillingAddressUri { get; set; }
+
+        [SerializationPropertyName("defaultShippingAddressUri")]
+        public string DefaultShippingAddressUri { get; set; }
+
+        [SerializationPropertyName("passwordSignature")]
+        public long PasswordSignature { get; set; }
+    }
+
+    public partial class EaEntitlementsResponse
+    {
+        [SerializationPropertyName("entitlements")]
+        public List<Entitlement> Entitlements { get; set; }
+    }
+
+    public partial class Entitlement
+    {
+        [SerializationPropertyName("entitlementId")]
+        public long EntitlementId { get; set; }
+
+        [SerializationPropertyName("offerId")]
+        public string OfferId { get; set; }
+
+        [SerializationPropertyName("entitlementTag")]
+        public string EntitlementTag { get; set; }
+
+        [SerializationPropertyName("grantDate")]
+        public DateTime GrantDate { get; set; }
+
+        [SerializationPropertyName("status")]
+        public string Status { get; set; }
+
+        [SerializationPropertyName("useCount")]
+        public long UseCount { get; set; }
+
+        [SerializationPropertyName("entitlementType")]
+        public string EntitlementType { get; set; }
+
+        [SerializationPropertyName("originPermissions")]
+        public string OriginPermissions { get; set; }
+
+        [SerializationPropertyName("updatedDate")]
+        public DateTimeOffset UpdatedDate { get; set; }
+
+        [SerializationPropertyName("productCatalog")]
+        public string ProductCatalog { get; set; }
+
+        [SerializationPropertyName("isConsumable")]
+        public bool IsConsumable { get; set; }
+
+        [SerializationPropertyName("version")]
+        public long Version { get; set; }
+
+        [SerializationPropertyName("suppressedOfferIds")]
+        public string[] SuppressedOfferIds { get; set; }
+
+        [SerializationPropertyName("suppressedBy")]
+        public string[] SuppressedBy { get; set; }
+
+        [SerializationPropertyName("offerType")]
+        public string OfferType { get; set; }
+
+        [SerializationPropertyName("originDisplayType")]
+        public string OriginDisplayType { get; set; }
+
+        [SerializationPropertyName("offerAccess")]
+        public string OfferAccess { get; set; }
+
+        [SerializationPropertyName("masterTitleId")]
+        public string MasterTitleId { get; set; }
+
+        [SerializationPropertyName("gameEditionTypeFacetKeyRankDesc")]
+        public string GameEditionTypeFacetKeyRankDesc { get; set; }
+    }
+}
--- source/Generic/PurchaseDateImporter/PurchaseDateImporter.cs
+++ source/Generic/PurchaseDateImporter/PurchaseDateImporter.cs
@@ -3,11 +3,15 @@ using Playnite.SDK.Data;
 using Playnite.SDK.Events;
 using Playnite.SDK.Plugins;
 using PlayniteUtilitiesCommon;
+using PluginsCommon.Web;
 using PurchaseDateImporter.Models;
 using System;
 using System.Collections.Generic;
+using System.Net;
+using System.Text;
 using System.Text.RegularExpressions;
 using System.Windows.Controls;
+using System.Xml;
 
 namespace PurchaseDateImporter
 {
@@ -88,7 +92,7 @@ namespace PurchaseDateImporter
                     Description = "Test",
                     MenuSection = "@Purchase Date Importer",
                     Action = a => {
-                        GetGogLicenses();
+                        GetOriginLicenses();
                     }
                 }
             };
@@ -427,6 +431,84 @@ namespace PurchaseDateImporter
 
             return licensesList;
         }
+
+        private List<LicenseData> GetOriginLicenses()
+        {
+            var licensesList = new List<LicenseData>();
+            var authResponse = GetEaAuthResponse();
+            if (authResponse == null)
+            {
+                return licensesList;
+            }
+
+            var entitlementsResponse = GetEaEntitlementsResponse(authResponse);
+            if (entitlementsResponse == null)
+            {
+                return licensesList;
+            }
+
+            foreach (var entitlement in entitlementsResponse.Entitlements)
+            {
+                var offerType = entitlement.OfferType.Replace(" ", "").ToLower();
+                if (offerType == "basegame" || offerType == "demo")
+                {
+                    licensesList.Add(new LicenseData(entitlement.OfferId, entitlement.GrantDate, entitlement.OfferId));
+                }
+            }
+
+            return licensesList;
+        }
+
+        private EaAuthResponse GetEaAuthResponse()
+        {
+            using (var webView = PlayniteApi.WebViews.CreateOffscreenView())
+            {
+                var authResponseUrl = @"https://accounts.ea.com/connect/auth?client_id=ORIGIN_JS_SDK&response_type=token&redirect_uri=nucleus:rest&prompt=none";
+                webView.NavigateAndWait(authResponseUrl);
+
+                var authJson = PlayniteUtilities.GetEmbeddedJsonFromWebViewSource(webView.GetPageSource());
+                if (authJson.StartsWith(@"{""error_code"""))
+                {
+                    // User is not logged in
+                    return null;
+                }
+
+                if (authJson.IsNullOrEmpty())
+                {
+                    return null;
+                }
+
+                return Serialization.FromJson<EaAuthResponse>(authJson);
+            }
+        }
+
+        private EaEntitlementsResponse GetEaEntitlementsResponse(EaAuthResponse authResponse)
+        {
+            using (var webClient = new WebClient { Encoding = Encoding.UTF8 })
+            {
+                //webClient.Headers.Add(HttpRequestHeader.Accept, "application/xml");
+                webClient.Headers.Add("Authorization", $"{authResponse.TokenType} {authResponse.AccessToken}");
+                webClient.Headers.Add("accept", "application/vnd.origin.v3+json; x-cache/force-write");
+
+                var identityResponse = webClient.DownloadString("https://gateway.ea.com/proxy/identity/pids/me");
+                if (identityResponse.IsNullOrEmpty())
+                {
+                    return null;
+                }
+
+                var identity = Serialization.FromJson<EaIdentityResponse>(identityResponse);
+
+                // For some reason somtimes the response is in XML format when the Headers contain the
+                // Authorization header
+                webClient.Headers.Clear();
+                webClient.Headers.Add("accept", "application/vnd.origin.v3+json; x-cache/force-write");
+                webClient.Headers.Add("authtoken", authResponse.AccessToken);
+                var url = string.Format("https://api1.origin.com/ecommerce2/consolidatedentitlements/{0}?machine_hash=1", identity.Pid.PidId);
+                var entitlementsResponseData = webClient.DownloadString(url);
+                return Serialization.FromJson<EaEntitlementsResponse>(entitlementsResponseData);
+            }
+        }
+
 
     }
 }
