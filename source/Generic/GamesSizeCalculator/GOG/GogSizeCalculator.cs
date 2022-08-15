0ea74582282cba82a2d5512e9ba33dd8e0766d95
Jeshibu <jeshibu@gmail.com>
2022-08-15 22:05:59 +0200

    GamesSizeCalculator: GOG skeleton

---
+++ source/Generic/GamesSizeCalculator/GOG/GogSizeCalculator.cs
@@ -0,0 +1,31 @@
+﻿using Playnite.SDK.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamesSizeCalculator.GOG
+{
+    public class GogSizeCalculator : IOnlineSizeCalculator
+    {
+        public string ServiceName { get; } = "GOG";
+
+        public async Task<ulong?> GetInstallSizeAsync(Game game)
+        {
+            var url = GetGogGameUrl(game);
+            if (url == null)
+            {
+                return null;
+            }
+
+            return 0UL;
+        }
+
+        private string GetGogGameUrl(Game game)
+        {
+            string url = game.Links?.Select(l => l.Url).FirstOrDefault(l => l.StartsWith("https://www.gog.com/game/"));
+            return url;
+        }
+    }
+}
