125846cc43d8919170951942bd44aa80aa8e39be
Brandon <brandon_corchado1@hotmail.com>
2023-08-21 22:39:00 -0600

    Create Char EqualsIgnoreCase extension method

---
+++ source/Common/PluginsCommon/Extensions/CharExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System
+{
+    public static class CharExtensions
+    {
+        public static bool EqualsIgnoreCase(this char char1, char char2)
+        {
+            return char.ToUpperInvariant(char1) == char.ToUpperInvariant(char2);
+        }
+    }
+}
