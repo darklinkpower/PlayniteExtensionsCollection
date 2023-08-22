dd0d40ee97461408f33d4ef48e3b7747e716e317
Brandon <brandon_corchado1@hotmail.com>
2023-08-21 20:23:12 -0600

    Metacritic: Remove default place holder settings

--- source/Metadata/MetacriticMetadata/MetacriticMetadataSettings.cs
+++ source/Metadata/MetacriticMetadata/MetacriticMetadataSettings.cs
@@ -10,16 +10,7 @@ namespace MetacriticMetadata
 {
     public class MetacriticMetadataSettings : ObservableObject
     {
-        private string option1 = string.Empty;
-        private bool option2 = false;
-        private bool optionThatWontBeSaved = false;
 
-        public string Option1 { get => option1; set => SetValue(ref option1, value); }
-        public bool Option2 { get => option2; set => SetValue(ref option2, value); }
-        // Playnite serializes settings object to a JSON object and saves it as text file.
-        // If you want to exclude some property from being saved then use `JsonDontSerialize` ignore attribute.
-        [DontSerialize]
-        public bool OptionThatWontBeSaved { get => optionThatWontBeSaved; set => SetValue(ref optionThatWontBeSaved, value); }
     }
 
     public class MetacriticMetadataSettingsViewModel : ObservableObject, ISettings
