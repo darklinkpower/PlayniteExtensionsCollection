5a3af7b0bc23606cb7943903f6874961471aa73f
Brandon <brandon_corchado1@hotmail.com>
2024-05-16 16:25:00 -0600

    Update release manifests

--- source/Generic/PlayState/Properties/AssemblyInfo.cs
+++ source/Generic/PlayState/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.74.0.0")]
-[assembly: AssemblyFileVersion("2.74.0.0")]
+[assembly: AssemblyVersion("2.75.0.0")]
+[assembly: AssemblyFileVersion("2.75.0.0")]
