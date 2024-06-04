cd272308c1198a0847e71c23ed5a02bff0f884c2
Brandon <brandon_corchado1@hotmail.com>
2024-06-04 07:23:51 -0600

    Create EventsCommon library

---
+++ source/Common/EventsCommon/EventAggregator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsCommon
+{
+    public class EventAggregator : IEventAggregator
+    {
+        private readonly Dictionary<Type, List<object>> _subscribers = new Dictionary<Type, List<object>>();
+
+        public void Subscribe<TEvent>(Action<TEvent> action)
+        {
+            if (!_subscribers.ContainsKey(typeof(TEvent)))
+            {
+                _subscribers[typeof(TEvent)] = new List<object>();
+            }
+
+            _subscribers[typeof(TEvent)].Add(action);
+        }
+
+        public void Publish<TEvent>(TEvent eventToPublish)
+        {
+            if (_subscribers.ContainsKey(typeof(TEvent)))
+            {
+                foreach (var subscriber in _subscribers[typeof(TEvent)])
+                {
+                    ((Action<TEvent>)subscriber)(eventToPublish);
+                }
+            }
+        }
+    }
+}
---
+++ source/Common/EventsCommon/IEventAggregator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsCommon
+{
+    public interface IEventAggregator
+    {
+        void Subscribe<TEvent>(Action<TEvent> action);
+        void Publish<TEvent>(TEvent eventToPublish);
+    }
+}
