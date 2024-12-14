d4df1bbb845c2ce3848bc11532796b8bc86f7249
Brandon <brandon_corchado1@hotmail.com>
2024-12-14 03:59:17 -0600

    SteamWishlistDiscountNotifier: Rework to make all remaining time updates use a single timer

--- source/Generic/SteamWishlistDiscountNotifier/Presentation/SteamWishlistViewerView.xaml.cs
+++ source/Generic/SteamWishlistDiscountNotifier/Presentation/SteamWishlistViewerView.xaml.cs
@@ -23,60 +23,65 @@ namespace SteamWishlistDiscountNotifier.Presentation
     /// </summary>
     public partial class SteamWishlistViewerView : UserControl
     {
-        private Dictionary<TextBlock, Timer> _timers = new Dictionary<TextBlock, Timer>();
+        private readonly HashSet<TextBlock> _textBlocksToUpdate = new HashSet<TextBlock>();
         private readonly Dispatcher _dispatcher;
+        private readonly Timer _timer;
 
         public SteamWishlistViewerView()
         {
             InitializeComponent();
-            this.Unloaded += SteamWishlistViewerView_Unloaded;
+            
             _dispatcher = System.Windows.Application.Current.Dispatcher;
+            _timer = new Timer(1000);
+            _timer.Start();
+            _timer.Elapsed += TimerElapsedHandler;
+            this.Unloaded += SteamWishlistViewerView_Unloaded;
+        }
+
+        private void TimerElapsedHandler(object sender, ElapsedEventArgs e)
+        {
+            UpdateTextBlocksText();
+        }
+
+        private void UpdateTextBlocksText()
+        {
+            _dispatcher.Invoke(() =>
+            {
+                foreach (var textBlock in _textBlocksToUpdate)
+                {
+                    var binding = textBlock.GetBindingExpression(TextBlock.TextProperty);
+                    binding?.UpdateTarget();
+                }
+            });
         }
 
         private void SteamWishlistViewerView_Unloaded(object sender, RoutedEventArgs e)
         {
             this.Unloaded -= SteamWishlistViewerView_Unloaded;
-        }
-
-        private void OnRemainingTimeTextBlockLoaded(object sender, RoutedEventArgs e)
-        {
-            if (sender is TextBlock textBlock && textBlock?.DataContext is SteamWishlistViewItem viewItem)
-            {
-                if (!_timers.ContainsKey(textBlock))
-                {
-                    var timer = new Timer(1000);
-                    timer.Elapsed += TimerElapsedHandler(textBlock);
-                    _timers[textBlock] = timer;
-                    timer.Start();
-                }
-
-                textBlock.Unloaded += TextBlockUnloadedHandler;
-            }
-        }
-
-        private ElapsedEventHandler TimerElapsedHandler(TextBlock textBlock)
-        {
-            return (ss, ee) =>
-            {
-                _dispatcher.Invoke(() =>
-                {
-                    var binding = textBlock.GetBindingExpression(TextBlock.TextProperty);
-                    binding?.UpdateTarget();
-                });
-            };
+            _timer.Elapsed -= TimerElapsedHandler;
+            _timer.Stop();
+            _textBlocksToUpdate.Clear();
         }
 
         private void TextBlockUnloadedHandler(object sender, RoutedEventArgs e)
         {
-            if (sender is TextBlock textBlock && _timers.ContainsKey(textBlock))
+            if (sender is TextBlock textBlock)
             {
-                var timer = _timers[textBlock];
-                timer.Elapsed -= TimerElapsedHandler(textBlock);
-                timer.Stop();
-                _timers.Remove(textBlock);
                 textBlock.Unloaded -= TextBlockUnloadedHandler;
+                if (_textBlocksToUpdate.Contains(textBlock))
+                {
+                    _textBlocksToUpdate.Remove(textBlock);
+                }
             }
         }
 
+        private void OnRemainingTimeTextBlockLoaded(object sender, RoutedEventArgs e)
+        {
+            if (sender is TextBlock textBlock && !_textBlocksToUpdate.Contains(textBlock))
+            {
+                _textBlocksToUpdate.Add(textBlock);
+                textBlock.Unloaded += TextBlockUnloadedHandler;
+            }
+        }
     }
 }
