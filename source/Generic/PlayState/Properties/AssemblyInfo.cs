7e6e84f5cd972ced03f98d8e568c23935be7a8d7
Brandon <brandon_corchado1@hotmail.com>
2025-04-05 01:47:37 -0600

    Update release manifests

--- source/Generic/PlayState/Properties/AssemblyInfo.cs
+++ source/Generic/PlayState/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("PlayState")]
-[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.78.0.0")]
-[assembly: AssemblyFileVersion("2.78.0.0")]
+[assembly: AssemblyVersion("2.79.0.0")]
+[assembly: AssemblyFileVersion("2.79.0.0")]
