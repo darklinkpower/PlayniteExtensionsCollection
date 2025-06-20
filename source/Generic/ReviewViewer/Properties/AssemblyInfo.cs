adff89a3d3ed48c620cde09e8a9c01fb5c01ec6b
Brandon <brandon_corchado1@hotmail.com>
2025-06-20 02:43:38 -0600

    Update release manifests

--- source/Generic/ReviewViewer/Properties/AssemblyInfo.cs
+++ source/Generic/ReviewViewer/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ReviewViewer")]
-[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.48.0.0")]
-[assembly: AssemblyFileVersion("2.48.0.0")]
+[assembly: AssemblyVersion("2.49.0.0")]
+[assembly: AssemblyFileVersion("2.49.0.0")]
