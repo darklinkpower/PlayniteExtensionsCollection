4959058bca2d1d34adefe65e4e30498aee151968
Brandon <brandon_corchado1@hotmail.com>
2025-06-23 00:11:34 -0600

    Update release manifests

--- source/Generic/PurchaseDateImporter/Properties/AssemblyInfo.cs
+++ source/Generic/PurchaseDateImporter/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("PurchaseDateImporter")]
-[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.27.0.0")]
-[assembly: AssemblyFileVersion("2.27.0.0")]
+[assembly: AssemblyVersion("2.28.0.0")]
+[assembly: AssemblyFileVersion("2.28.0.0")]
